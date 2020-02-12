505b0d2c3b9def6dc4352ac34ddb6d8cf91e0132
Isispaglia <isis_paglia@hotmail.com>
2020-02-12 10:27:29 -0300

    arquivos excluidos no gitignore

---
+++ script.R
@@ -0,0 +1,6 @@
+#script para ler a tabela limpa#
+#os dados originais estão em xlsx#
+
+read.table("./data/dados_iris.txt")
+read.table("data/dados_iris.txt")
+dados_proc <- read.table("data/dados_iris.txt")
